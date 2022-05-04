653ef4339b58b3d6e80ec7568bff978d2ffbd232
Kilian Brachtendorf <Kilian.Brachtendorf@t-online.de>
2022-05-05 01:28:30 +0200

    add build files to repo

---
+++ lib/core/QROptions.d.ts
@@ -0,0 +1,29 @@
+import { DotType, Options, TypeNumber, ErrorCorrectionLevel, Mode, DrawType, Gradient } from '../types';
+export interface RequiredOptions extends Options {
+    type: DrawType;
+    width: number;
+    height: number;
+    margin: number;
+    data: string;
+    qrOptions: {
+        typeNumber: TypeNumber;
+        mode?: Mode;
+        errorCorrectionLevel: ErrorCorrectionLevel;
+    };
+    imageOptions: {
+        hideBackgroundDots: boolean;
+        imageSize: number;
+        margin: number;
+    };
+    dotsOptions: {
+        type: DotType;
+        color: string;
+        gradient?: Gradient;
+    };
+    backgroundOptions: {
+        color: string;
+        gradient?: Gradient;
+    };
+}
+declare const defaultOptions: RequiredOptions;
+export default defaultOptions;
---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import QRCodeCanvas from './core/QRCanvas.js';
+import dotTypes from './constants/dotTypes.js';
+import cornerDotTypes from './constants/cornerDotTypes.js';
+import cornerSquareTypes from './constants/cornerSquareTypes.js';
+import errorCorrectionLevels from './constants/errorCorrectionLevels.js';
+import errorCorrectionPercents from './constants/errorCorrectionPercents.js';
+import modes from './constants/modes.js';
+import qrTypes from './constants/qrTypes.js';
+import drawTypes from './constants/drawTypes.js';
+export { dotTypes, cornerDotTypes, cornerSquareTypes, errorCorrectionLevels, errorCorrectionPercents, modes, qrTypes, drawTypes };
+export default QRCodeCanvas;
